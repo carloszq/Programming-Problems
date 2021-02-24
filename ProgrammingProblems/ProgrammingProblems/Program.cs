52e6c955834b915a0726873207355b55513533a9
Carlos Zepeda <czq1989@hotmail.com>
2021-02-24 11:45:19 -0800

    Resolve leet code #14 (#3)
    Co-authored-by: Carlos Zepeda <cquinta@microsoft.com>

--- ProgrammingProblems/ProgrammingProblems/Program.cs
+++ ProgrammingProblems/ProgrammingProblems/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammingProblems.LeetCodeUtil;
+using System;
 
 namespace ProgrammingProblems
 {
@@ -7,14 +8,8 @@ namespace ProgrammingProblems
         static void Main(string[] args)
         {
             // Test code here.
-            Console.WriteLine($"Roman numeral to integer (III -> {LeetCode.RomanToInt("I")})");
-            Console.WriteLine($"Roman numeral to integer (III -> {LeetCode.RomanToInt("III")})");
-            Console.WriteLine($"Roman numeral to integer (IV -> {LeetCode.RomanToInt("IV")})");
-            Console.WriteLine($"Roman numeral to integer (IX -> {LeetCode.RomanToInt("IX")})");
-            Console.WriteLine($"Roman numeral to integer (XI -> {LeetCode.RomanToInt("XI")})");
-            Console.WriteLine($"Roman numeral to integer (XIII -> {LeetCode.RomanToInt("XIII")})");
-            Console.WriteLine($"Roman numeral to integer (XIV -> {LeetCode.RomanToInt("XIV")})");
-            Console.WriteLine($"Roman numeral to integer (C -> {LeetCode.RomanToInt("C")})");
+            var strs = new string[] { "flower", "flow", "flight" };
+            Console.WriteLine($"Longest Common Prefix: {StringUtil.LongestCommonPrefix(strs)}");
         }    
     }
 }
